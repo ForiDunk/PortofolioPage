db67da56a237c8b19caacd325eb4176a1a37d4d1
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-06-17 20:01:23 +0300

    remove firebase and authentification and some minor changes

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import Projects from './Projects';
 import Introduction from './Introduction';
 import Footer from './Footer';
 
 const DashboardPage = () => (
   <div>
+    <Header />
     <Introduction />
     <Projects />
     <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { startLogout } from '../actions/auth';
 
-export const Header = ({ startLogout }) => (
+const Header = () => (
   <header className="header">
     <div className="content-container">
       <div className="header__content">
-        <Link className="header__title" to="/dashboard">
+        <Link className="header__title" to="/">
           <h1>Szabolcs Forreiter</h1>
         </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
+        <button className="button button--link" href="#">About</button>
+        <button className="button button--link" href="#">Projects</button>
+        <button className="button button--link" href="#">Contact</button>
       </div>
     </div>
   </header>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-  startLogout: () => dispatch(startLogout())
-});
-
-export default connect(undefined, mapDispatchToProps)(Header);
+export default Header;
