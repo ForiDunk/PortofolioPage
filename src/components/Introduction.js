671b81763cbb51a93dcf766b1de1e1c1c855077d
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-06-18 21:10:50 +0300

    add some text to Introduction component

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Introduction = () => (
   <div className="page-header">
     <div className="content-container">
-      <h1>Introduction content</h1>
-      <p>about me</p>
-      <p>a few pictures (maybe)</p>
+      <h1>Hello!</h1>
+      <h3>All good things start out you challanging yourself...</h3>
+      <p>...like "I wonder if I can learn to code..."</p>
+      <p>...than you try...</p> 
+      <p>...and people say "you can't..."</p>
+      <p>...and after like a year of constant learning, I present you my portofolio page.</p>
       <a href="#"><p>downloadable cv</p></a>
     </div>
   </div>
