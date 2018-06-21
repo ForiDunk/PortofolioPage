8db192aec2182c902106da10153d6bfe3d541a27
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-06-21 23:20:07 +0300

    update drawer button

--- src/components/CV.js
+++ src/components/CV.js
@@ -8,9 +8,6 @@ const styles = {
     flexDirection: "column",
     alignItems: "center",
     margin: 20
-  },
-  button: {
-    margin: 7
   }
 };
 
@@ -18,10 +15,10 @@ export default () => (
   <div style={styles.div}>
     <Typography variant="headline">Thank you for visiting my page!</Typography>
     <Typography variant="caption">
-      You can download my CV by clicking on the button below:
+      You can download my CV by clicking on the icon below:
     </Typography>
     <Tooltip title="Download CV">
-      <Button style={styles.button} variant="fab" color="secondary">
+      <Button style={styles.button} size="small" color="secondary">
       <FileDownload />
       </Button>
     </Tooltip>
