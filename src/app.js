d94b663802eb8c66824f907c9a012932a352f401
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-06-27 20:48:05 +0300

    fix landing page margin

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import 'App.css';
+import './App.css';
 
 const theme = createMuiTheme({
   typography: {
